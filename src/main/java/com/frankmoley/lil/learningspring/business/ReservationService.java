f69930ed37891ab5525247684a281054f36d5138
ojrealdev <stephenojwang11@gmail.com>
2022-06-24 02:15:24 +0300

    optimized reservation service attributes from autowiring to making them final and adding a contructor for the attributes

--- src/main/java/com/frankmoley/lil/learningspring/business/ReservationService.java
+++ src/main/java/com/frankmoley/lil/learningspring/business/ReservationService.java
@@ -18,12 +18,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ReservationService {
-    @Autowired
-    private RoomRepository roomRepository;
-    @Autowired
-    private GuestRepository guestRepository;
-    @Autowired
-    private ReservationRepository reservationRepository;
+    private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+    private final ReservationRepository reservationRepository;
+
+    public ReservationService(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
+        this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+        this.reservationRepository = reservationRepository;
+    }
 
     public List<RoomReservation> getRoomReservationsForDate(Date date) {
         Iterable<Room> rooms = this.roomRepository.findAll();
